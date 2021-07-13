0838bdb9bdbef0c32b80c485d96f01554bd99ff9
Koki Satani <n20010@std.it-college.ac.jp>
2021-07-13 03:09:50 +0000

    Add jQuery feature

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
+// for jquery
+// 以下追記
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
+    })
+)
+// ここまで
+
 module.exports = environment
