c541b669d9ea1f341d949681fafe5b87d67e04ff
Koki Satani <n20010@std.it-college.ac.jp>
2021-07-26 13:13:35 +0000

    add a button for twitter feature

--- app/javascript/my_jQuery/changetwittercomments.js
+++ app/javascript/my_jQuery/changetwittercomments.js
@@ -1,25 +1,58 @@
 /* global $ */
 $(document).ready(function(){
   var count = 0;
-  $(function(){
-    setInterval(update, 7000);
-    //10000ミリ秒ごとにupdateという関数を実行する
+  var tweets;
+  
+  $("#button").click(function(){
+    if ($(this).hasClass("btn--deactive")) {
+      
+      $(this).removeClass("btn--deactive");
+      $(this).addClass("btn--active");
+      $(this).html("Twitterのサーチを止める");
+      update();
+      
+    } else {
+      
+      $(this).removeClass("btn--active");
+      $(this).addClass("btn--deactive");
+      $(this).html("Twitterのサーチを始める");
+      stop_update();
+      $('<div class="btn btn--orange btn--radius btn--reset">Clear</div>')
+        .appendTo(".btn-container").hide().fadeIn(500);
+      
+    }
+  });
+
+  function update(){ //この関数では以下のことを行う
+    tweets = setInterval(function() {
+      var latest_tweet_id = $('span').text();
+  
+      $.ajax({ //ajax通信で以下のことを行う
+        url: 'twitter', //urlは現在のページを指定
+        type: 'GET', //メソッドを指定
+        data: { //railsに引き渡すデータは
+          'latest_tweet_id': latest_tweet_id //このような形(paramsの形をしています)で、'id'には'message_id'を入れる
+        },
+        dataType: 'script'
+      });
+      
+      count = count + 1;
+      $('h3').html('jQueryは' + String(count) + '回実行されました');
+    }, 7000);
+    
+  }
+  
+  function stop_update(){
+    clearInterval(tweets);
+  }
+  
+  $("body").on("click", ".btn--reset", function() {
+    $("#tweets").fadeOut().empty().fadeIn();
+    $(this).remove();
+    $('h3').html('まだjQueryは実行されていません');
+    count = 0;
+    $('span').html(count);
   });
   
-  function update(){ //この関数では以下のことを行う
-    var latest_tweet_id = $('span').text();
-
-    $.ajax({ //ajax通信で以下のことを行う
-      url: 'twitter', //urlは現在のページを指定
-      type: 'GET', //メソッドを指定
-      data: { //railsに引き渡すデータは
-        'latest_tweet_id': latest_tweet_id //このような形(paramsの形をしています)で、'id'には'message_id'を入れる
-      },
-      dataType: 'script'
-    });
-    
-    count = count + 1;
-    $('h3').html('jQueryは' + String(count) + '回実行されました');
-     
-  }
+  
 });
