be092f2fa0bb20052342efd465764b6478033a05
Koki Satani <n20010@std.it-college.ac.jp>
2021-12-07 12:38:53 +0000

    improve hastag regex function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -68,7 +68,7 @@ class ApplicationController < ActionController::Base
         
         if keyword.match(/^#.*$/)
           keyword = keyword.gsub(/#/, '')
-          text_regexed = text_regexed.gsub(/##{keyword}/, ' ')
+          text_regexed = text_regexed.gsub(/##{keyword}[ 　\s\S]/, ' ')
         else
         
           
